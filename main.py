b255f34f81885ceea320ba8f93976809e0b899e3
ToinonPolytech <toinon.georget@waapi.fr>
2023-04-10 17:22:27 +0200

    update cron

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ client = WebClient(token=slack_api_token)
 # Récupération des 5 messages les plus réactifs de la semaine dernière
 start_of_week = (datetime.datetime.now() - datetime.timedelta(days=7)).timestamp()
 try:
-    response = client.conversations_history(channel="troll", oldest=start_of_week)
+    response = client.conversations_history(channel="#troll", oldest=start_of_week)
+	 print("response["messages"])
     messages = response['messages']
     messages.sort(key=lambda x: len(x['reactions']), reverse=True)
     top_messages = messages[:5]
@@ -32,7 +33,7 @@ for i, msg in enumerate(top_messages):
 # Envoi du message sur Slack
 try:
     response = client.chat_postMessage(
-        channel="troll",
+        channel="#troll",
         text=message
     )
     print("Message envoyé : ", response['ts'])
