a73ec53ca304b54ca06540236a67cf8059848042
ToinonPolytech <toinon.georget@waapi.fr>
2023-04-10 17:12:12 +0200

    update cron

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from subprocess import call
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('cron', day_of_week='mon', hour=16,minute=12)
+@scheduler.scheduled_job('cron', day_of_week='mon', hour=15,minute=13)
 def scheduled_job():
     call(['python', 'main.py'])
 
