517b64f1850f7d4c2cae9ed33f976ab2f78b0232
ToinonPolytech <toinon.georget@waapi.fr>
2023-04-10 16:31:39 +0200

    ajout du script python

--- main.py
+++ main.py
@@ -1,64 +1,29 @@
 import os
-from slack_bolt import App
-from datetime import datetime, timedelta
+import datetime
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-app = App(token=os.environ["SLACK_BOT_TOKEN"])
+# Initialisation du client Slack
+client = WebClient(token=os.environ['xapp-1-A052K8KNA3W-5098006720785-9b0c659a79fc7c663666e05f89de4413da798fa90e0cb994317a10a512c8b684'])
 
-# Fonction pour trier les messages par nombre de réactions
-def sort_by_reaction_count(messages):
-    sorted_messages = sorted(messages, key=lambda x: len(x['reactions']), reverse=True)
-    return sorted_messages[:5]
+# Récupération des 5 messages les plus réactifs de la semaine dernière
+start_of_week = (datetime.datetime.now() - datetime.timedelta(days=7)).timestamp()
+response = client.conversations_history(channel="#troll", oldest=start_of_week)
+messages = response['messages']
+messages.sort(key=lambda x: len(x['reactions']), reverse=True)
+top_messages = messages[:5]
 
-# Fonction pour récupérer les messages de la semaine dernière
-def get_last_week_messages(channel_id):
-    last_week = (datetime.today() - timedelta(days=7)).strftime('%s')
-    try:
-        response = app.client.conversations_history(
-            channel=channel_id,
-            oldest=last_week
-        )
-        return response['messages']
-    except SlackApiError as e:
-        print("Error retrieving messages: {}".format(e))
+# Construction du message à envoyer sur Slack
+message = "Les 5 messages les plus réactifs de la semaine dernière dans le canal #troll sont : \n\n"
+for i, msg in enumerate(top_messages):
+    message += f"{i+1}. <{msg['permalink']}|{msg['text']}> ({len(msg['reactions'])} réactions)\n"
 
-# Fonction pour créer le classement des messages sous forme de blocs Slack
-def create_leaderboard_blocks(messages):
-    leaderboard_blocks = [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Classement des 5 messages les plus populaires de la semaine dernière :*"
-            }
-        }
-    ]
-    for i, m in enumerate(messages):
-        leaderboard_blocks.append(
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"{i+1}. <{m['permalink']}|{m['text']}> - {len(m['reactions'])} réactions"
-                }
-            }
-        )
-    return leaderboard_blocks
-
-# Envoi du message de classement chaque lundi à 9h
-@app.schedule("cron", hour="9", day_of_week="1")
-def send_leaderboard_message():
-    # Récupérer les messages du canal
-    messages = get_last_week_messages(channel_id='#general')
-    # Trier les messages par nombre de réactions
-    sorted_messages = sort_by_reaction_count(messages)
-    # Créer les blocs de classement
-    leaderboard_blocks = create_leaderboard_blocks(sorted_messages)
-    # Envoyer le message dans le canal
-    response = app.client.chat_postMessage(
-        channel='#troll',
-        blocks=leaderboard_blocks
+# Envoi du message sur Slack
+try:
+    response = client.chat_postMessage(
+        channel="#troll",
+        text=message
     )
-    print(response)
-
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    print("Message envoyé : ", response['ts'])
+except SlackApiError as e:
+    print("Erreur : {}".format(e))
