d543d4670bd226f0067be9c1f00e3d071d340851
ToinonPolytech <toinon.georget@waapi.fr>
2023-04-10 16:49:04 +0200

    v0 ready

--- clock.py
+++ clock.py
@@ -3,8 +3,8 @@ from subprocess import call
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('cron', day_of_week='mon', hour=9)
+@scheduler.scheduled_job('cron', day_of_week='mon', hour=15,minute=43)
 def scheduled_job():
-    call(['python', 'NOM_DE_VOTRE_SCRIPT.py'])
+    call(['python', 'main.py'])
 
 scheduler.start()
